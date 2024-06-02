ab68e029bc3b4f35dcdb79053660f42ae705c315
tristanphips <tristan.phipson@bbd.co.za>
2024-06-02 21:46:33 +0200

    addedcustomdaomiain

--- bin/task-progress-infra.ts
+++ bin/task-progress-infra.ts
@@ -10,7 +10,8 @@ new TaskProgressInfraStack(app, 'TaskProgressInfraStack', {
   dbUsername: 'tpb_db_user',
   orgName: 'SecurityLevelup-TaskProgressBoard',
   repoName: 'task-progress-infra',
-  domainNames: ['taskify.phipson.co.za'],
+  domainNames: ['taskify.phipson.co.za', 'login.taskify.phipson.co.za'],
   certificateArn: 'arn:aws:acm:us-east-1:978251882572:certificate/841bcf51-a095-454a-abd2-d312833ee2d6',
   apiCertArn: 'arn:aws:acm:us-east-1:978251882572:certificate/551964f2-ac30-48d2-b67c-05f61a5cba11',
+  loginCertArn: 'arn:aws:acm:us-east-1:978251882572:certificate/f01f3cac-f425-4a81-a93b-38ef75d2dfd9',
 });
